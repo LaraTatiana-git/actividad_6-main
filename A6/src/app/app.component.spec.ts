44625af0ae106ce81c457ebc277490d736ca7135
Lara Tatiana <laratatiana2024@gmail.com>
2024-12-22 00:30:08 +0100

    Update app.component.spec.ts

--- A6/src/app/app.component.spec.ts
+++ A6/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'A6_GMC' title`, () => {
+  it(`should have the 'A6' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('A6_GMC');
+    expect(app.title).toEqual('A6');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, A6_GMC');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Actividad_6');
   });
 });
